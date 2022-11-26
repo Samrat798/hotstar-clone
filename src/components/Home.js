17acdb6df52dc22e6219d1216c14fedcaea44da2
Samrat Majumder <majumdersamrat26@gamil.com>
2022-11-26 12:55:52 +0530

    aster

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
 import NewDisney from "./NewDisney";
@@ -6,8 +6,47 @@ import Original from "./Original";
 import Recommends from "./Recommends";
 import Trending from "./Trending";
 import Viewers from "./Viewers";
+import { useDispatch, useSelector } from "react-redux";
+import db from "../firebase";
+import { setMovies } from "../features/movieSlice";
+import { selectUserName } from "../features/userSlice";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  let recommends = [];
+  let newDisneys = [];
+  let originals = [];
+  let trending = [];
+
+  useEffect(() => {
+    db.collection("movies").onSnapshot((snapShot) => {
+      snapShot.docs.map((doc) => {
+        switch (doc.data().type) {
+          case "recommend":
+            recommends.push({ id: doc.id, ...doc.data() });
+            break;
+          case "new":
+            newDisneys.push({ id: doc.id, ...doc.data() });
+            break;
+          case "original":
+            originals.push({ id: doc.id, ...doc.data() });
+            break;
+          case "trending":
+            trending.push({ id: doc.id, ...doc.data() });
+            break;
+        }
+      });
+      dispatch(
+        setMovies({
+          recommend: recommends,
+          newDisney: newDisneys,
+          original: originals,
+          trending: trending,
+        })
+      );
+    });
+  }, [userName]);
   return (
     <Container>
       <ImgSlider />
